03117f9082eeb6f883448996ac1cc02e1e9086e4
Yehor <99051328+R0BIK@users.noreply.github.com>
2024-11-17 21:10:33 +0200

    Add files via upload

---
+++ lab4/script.js
@@ -0,0 +1,10 @@
+﻿const panel = document.querySelector('.footer');
+window.addEventListener('mousemove', (event) => {
+    const mouseY = event.clientY;
+    if (mouseY >= window.innerHeight - 80 && window.innerWidth > 800) {
+        panel.classList.add('show');
+    } else {
+        panel.classList.remove('show');
+    }
+});
+
